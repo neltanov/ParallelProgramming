560b67a10587fcad04536340fcf630946af46e97
Bayarto Neltanov <bayarto2003@gmail.com>
2023-03-15 22:58:49 +0700

    second program works

--- lab2/main.c
+++ lab2/main.c
@@ -3,11 +3,11 @@
 #include <math.h>
 #include <omp.h>
 
-#define N 20000
+#define N 18000
 #define EPS 0.0000001
 #define TAU 0.00001
 
-void initMatrix(double *matrix, int size) {
+void init_matrix(double *matrix, int size) {
     int line = 0;
     for (int i = 0; i < size * size; i++) {
         if (i == line * size + line) {
@@ -19,13 +19,13 @@ void initMatrix(double *matrix, int size) {
     }
 }
 
-void initRightPart(double *array, int size) {
+void init_right_part(double *array, int size) {
     for (int i = 0; i < size; i++) {
         array[i] = (double) size + 1;
     }
 }
 
-void printMatrix(double *matrix, int size) {
+void print_matrix(double *matrix, int size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             printf("%0.3lf ", matrix[i * size + j]);
@@ -34,14 +34,14 @@ void printMatrix(double *matrix, int size) {
     }
 }
 
-void printArray(double *array, int size) {
+void print_array(double *array, int size) {
     for (int i = 0; i < size; i++) {
         printf("%0.3lf ", array[i]);
     }
     printf("\n");
 }
 
-double euclideanNorm(const double *vec, int size) {
+double euclidean_norm(const double *vec, int size) {
     double res = 0;
     #pragma omp parallel for
     for (int i = 0; i < size; i++)
@@ -49,7 +49,7 @@ double euclideanNorm(const double *vec, int size) {
     return sqrt(res);
 }
 
-void mulMatVec(const double *matrix, const double *vec, int size, double *res) {
+void mul_mat_vec(const double *matrix, const double *vec, int size, double *res) {
     #pragma omp parallel for
     for (int i = 0; i < size; i++) {
         res[i] = 0;
@@ -59,31 +59,31 @@ void mulMatVec(const double *matrix, const double *vec, int size, double *res) {
     }
 }
 
-void subVectors(double *a, const double *b, int size) {
+void sub_vectors(double *a, const double *b, int size) {
     #pragma omp parallel for
     for (int i = 0; i < size; i++) {
         a[i] = a[i] - b[i];
     }
 }
 
-void mulNumVec(double num, double *vec, int size) {
+void mul_num_vec(double num, double *vec, int size) {
     #pragma omp parallel for
     for (int i = 0; i < size; i++) {
         vec[i] = num * vec[i];
     }
 }
 
-void singleIterate(double *A, double *x, double *b, int size) {
+void single_iterate(double *A, double *x, double *b, int size) {
     double *res = (double *) malloc(size * sizeof(double));
     double criteria = 1;
 
     while (criteria >= EPS) {
-        mulMatVec(A, x, size, res);
-        subVectors(res, b, size);
-        criteria = euclideanNorm(res, size) / euclideanNorm(b, size);
+        mul_mat_vec(A, x, size, res);
+        sub_vectors(res, b, size);
+        criteria = euclidean_norm(res, size) / euclidean_norm(b, size);
         printf("norm: %0.15lf\n", criteria);
-        mulNumVec(TAU, res, size);
-        subVectors(x, res, size);
+        mul_num_vec(TAU, res, size);
+        sub_vectors(x, res, size);
     }
 
     free(res);
@@ -92,15 +92,14 @@ void singleIterate(double *A, double *x, double *b, int size) {
 int main(void) {
     double time_begin = omp_get_wtime();
     int m_size = N;
-
     double *A = (double *) malloc(m_size * m_size * sizeof(double));
-    initMatrix(A, m_size);
+    init_matrix(A, m_size);
     double *b = (double *) malloc(m_size * sizeof(double));
-    initRightPart(b, m_size);
+    init_right_part(b, m_size);
     double *x = (double *) calloc(m_size, sizeof(double));
 
-    singleIterate(A, x, b, m_size);
-    printArray(x, m_size);
+    single_iterate(A, x, b, m_size);
+    print_array(x, m_size);
 
     free(A);
     free(b);
--- lab2/main2.c
+++ lab2/main2.c
@@ -7,7 +7,7 @@
 #define EPS 0.0000001
 #define TAU 0.00001
 
-void initMatrix(double *matrix, int size) {
+void init_matrix(double *matrix, int size) {
     int line = 0;
     for (int i = 0; i < size * size; i++) {
         if (i == line * size + line) {
@@ -19,13 +19,13 @@ void initMatrix(double *matrix, int size) {
     }
 }
 
-void initRightPart(double *array, int size) {
+void init_right_part(double *array, int size) {
     for (int i = 0; i < size; i++) {
         array[i] = (double) size + 1;
     }
 }
 
-void printMatrix(double *matrix, int size) {
+void print_matrix(double *matrix, int size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             printf("%0.3lf ", matrix[i * size + j]);
@@ -34,52 +34,62 @@ void printMatrix(double *matrix, int size) {
     }
 }
 
-void printArray(double *array, int size) {
+void print_array(double *array, int size) {
     for (int i = 0; i < size; i++) {
         printf("%0.3lf ", array[i]);
     }
     printf("\n");
 }
 
-double euclideanNorm(const double *vec, int size) {
+double euclidean_norm(const double *vec, int size) {
     double res = 0;
+#pragma omp parallel for default(none) shared(size, vec) reduction(+:res)
     for (int i = 0; i < size; i++)
         res += vec[i] * vec[i];
     return sqrt(res);
 }
 
-void mulMatVec(const double *matrix, const double *vec, int size, double *res) {
-#pragma omp parallel for
+void mul_mat_vec(const double *matrix, const double *vec, int size, double *res) {
+#pragma omp for
     for (int i = 0; i < size; i++) {
         res[i] = 0;
         for (int j = 0; j < size; j++) {
-            res[i] += matrix[i * size + j] * vec[j];
+                res[i] += matrix[i * size + j] * vec[j];
         }
     }
 }
 
-void subVectors(double *a, const double *b, int size) {
+void sub_vectors(double *a, const double *b, int size) {
+#pragma omp for
     for (int i = 0; i < size; i++) {
         a[i] = a[i] - b[i];
     }
 }
 
-void mulNumVec(double num, double *vec, int size) {
+void mul_num_vec(double num, double *vec, int size) {
+#pragma omp for
     for (int i = 0; i < size; i++) {
         vec[i] = num * vec[i];
     }
 }
 
-void singleIterate(double *A, double *x, double *b, int size) {
+void single_iterate(double *A, double *x, double *b, int size) {
     double *res = (double *) malloc(size * sizeof(double));
     double criteria = 1;
+    double euclidean_norm_b = euclidean_norm(b, size);
+    #pragma omp parallel default(none) shared(A, x, b, size, res, euclidean_norm_b, criteria)
+    {
     while (criteria >= EPS) {
-        mulMatVec(A, x, size, res);
-        subVectors(res, b, size);
-        criteria = euclideanNorm(res, size) / euclideanNorm(b, size);
-        printf("norm: %0.15lf\n", criteria);
-        mulNumVec(TAU, res, size);
-        subVectors(x, res, size);
+            mul_mat_vec(A, x, size, res);
+            sub_vectors(res, b, size);
+            #pragma omp single
+            {
+                criteria = euclidean_norm(res, size) / euclidean_norm_b;
+                printf("norm: %0.15lf\n", criteria);
+            }
+            mul_num_vec(TAU, res, size);
+            sub_vectors(x, res, size);
+        }
     }
 
     free(res);
@@ -90,15 +100,14 @@ int main(void) {
     int m_size = N;
 
     double *A = (double *) malloc(m_size * m_size * sizeof(double));
-    initMatrix(A, m_size);
+    init_matrix(A, m_size);
     double *b = (double *) malloc(m_size * sizeof(double));
-    initRightPart(b, m_size);
+    init_right_part(b, m_size);
     double *x = (double *) calloc(m_size, sizeof(double));
 
-    singleIterate(A, x, b, m_size);
+    single_iterate(A, x, b, m_size);
 
-    printArray(x, m_size);
-
+    print_array(x, m_size);
     free(A);
     free(b);
     free(x);
