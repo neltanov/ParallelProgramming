0aa1471a75c7323106ccc67310ae01bbac672562
Bayarto Neltanov <bayarto2003@gmail.com>
2023-04-13 10:38:51 +0700

    Add time counting Fix code style

--- lab3/main.c
+++ lab3/main.c
@@ -4,6 +4,7 @@
 #include <mpi.h>
 
 #define RANK_ROOT 0
+#define LOWER_BOUND 0
 
 void fill_matrix(double *matrix, int rows, int cols) {
     for (int i = 0; i < rows; i++) {
@@ -34,19 +35,22 @@ void multiply_matrices(const double *A, const double *B, double *C, int n1, int 
 }
 
 int run(void) {
-    int dims[2] = {0, 0}, periods[2] = {0, 0}, coords[2], reorder = 1;
     int size, rank, sizey, sizex, ranky, rankx;
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    const double start_time_s = MPI_Wtime();
+
+    int dims[2] = {0, 0}, periods[2] = {0, 0}, coords[2], reorder = 1;
 
     MPI_Dims_create(size, 2, dims);
     sizex = dims[0];
     sizey = dims[1];
 
     MPI_Comm comm2d;
-    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &comm2d); // создание 2d решетки
-    MPI_Comm_rank(comm2d, &rank); // получение номеров процесса в 2d решетке (старая нумерация)
-    MPI_Cart_get(comm2d, 2, dims, periods, coords); // получение номера процесса в виде координат решетки
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &comm2d);
+    MPI_Cart_get(comm2d, 2, dims, periods, coords);
     rankx = coords[0];
     ranky = coords[1];
 
@@ -58,8 +62,8 @@ int run(void) {
     MPI_Comm_split(comm2d, ranky, rankx, &commAbscissa);
     MPI_Comm_split(comm2d, rankx, ranky, &commOrdinate);
 
-    int n1 = 4, n2 = 4, n3 = 6;
-    if (n3 % sizex != 0) {
+    int n1 = 100, n2 = 100, n3 = 100;
+    if (n1 % sizey != 0 || n3 % sizex != 0) {
         printf("%d mod %d != 0", n3, sizex);
         return 1;
     }
@@ -70,10 +74,6 @@ int run(void) {
 
     const int rows_per_process = n1 / sizey;
     const int columns_per_process = n3 / sizex;
-
-    double *part_A = calloc(rows_per_process * n2, sizeof(double));
-    double *part_B = calloc(n2 * columns_per_process, sizeof(double));
-    double *part_C = calloc(rows_per_process * columns_per_process, sizeof(double));
 
     if (RANK_ROOT == rank) {
         A = calloc(n1 * n2, sizeof(double));
@@ -88,6 +88,10 @@ int run(void) {
         printf("\n");
     }
 
+    double *part_A = calloc(rows_per_process * n2, sizeof(double));
+    double *part_B = calloc(n2 * columns_per_process, sizeof(double));
+    double *part_C = calloc(rows_per_process * columns_per_process, sizeof(double));
+
     if (RANK_ROOT == rankx) {
         MPI_Scatter(A, rows_per_process * n2, MPI_DOUBLE,
                      part_A, rows_per_process * n2, MPI_DOUBLE, RANK_ROOT, commOrdinate);
@@ -95,76 +99,31 @@ int run(void) {
 
     MPI_Bcast(part_A, rows_per_process * n2, MPI_DOUBLE, RANK_ROOT, commAbscissa);
 
-//    sleep(rank);
-//    printf("Part of matrix A. Rank: (%d, %d)\n", rankx, ranky);
-//    print_matrix(part_A, rows_per_process, n2);
-//    printf("\n");
-
-
     MPI_Datatype vertical_slice;
-    MPI_Type_vector(
-            /* blocks count - number of rows */ n2,
-            /* block length  */ columns_per_process,
-            /* stride - block start offset */ n3,
-            /* old type - element type */ MPI_DOUBLE,
-            /* new type */ &vertical_slice
-    );
+    MPI_Type_vector(n2, columns_per_process, n3, MPI_DOUBLE, &vertical_slice);
     MPI_Type_commit(&vertical_slice);
 
     MPI_Datatype vertical_slice_resized;
-    MPI_Type_create_resized(
-            vertical_slice,
-            /* lower bound */ 0,
-            /* extent - size in bytes */ (int) (columns_per_process * sizeof(double)),
-            /* new type */ &vertical_slice_resized
-    );
+    MPI_Type_create_resized(vertical_slice, LOWER_BOUND,
+                            (int) (columns_per_process * sizeof(double)), &vertical_slice_resized);
     MPI_Type_commit(&vertical_slice_resized);
 
     if (RANK_ROOT == ranky) {
-        MPI_Scatter(
-                /* send buffer */ B,
-                /* number of <send data type> elements sent */ 1,
-                /* send data type */ vertical_slice_resized,
-                /* recv buffer */ part_B,
-                /* number of <recv data type> elements received */ n2 * columns_per_process,
-                /* recv data type */ MPI_DOUBLE,
-                                  RANK_ROOT,
-                                  commAbscissa
-        );
+        MPI_Scatter(B, 1, vertical_slice_resized,
+                    part_B, n2 * columns_per_process, MPI_DOUBLE, RANK_ROOT, commAbscissa);
     }
     MPI_Bcast(part_B, n2 * columns_per_process, MPI_DOUBLE, RANK_ROOT, commOrdinate);
 
-//    sleep(size + rank);
-//    printf("Part of matrix B. Rank: (%d, %d)\n", rankx, ranky);
-//    print_matrix(part_B, n2, columns_per_process);
-//    printf("\n");
-//
-//    sleep(rank);
     multiply_matrices(part_A, part_B, part_C, rows_per_process, n2, columns_per_process);
 
-//    sleep(2 * size + rank);
-//    printf("Part of matrix C. Rank: (%d, %d)\n", rankx, ranky);
-//    print_matrix(part_C, rows_per_process, columns_per_process);
-//    printf("\n");
 
     MPI_Datatype matrix_block;
-    MPI_Datatype matrix_block_resized;
-
-    MPI_Type_vector(
-            /* blocks count - number of rows */ rows_per_process,
-            /* block length  */ columns_per_process,
-            /* stride - block start offset */ n3,
-            /* old type - element type */ MPI_DOUBLE,
-            /* new type */ &matrix_block
-    );
+    MPI_Type_vector(rows_per_process, columns_per_process, n3, MPI_DOUBLE, &matrix_block);
     MPI_Type_commit(&matrix_block);
 
-    MPI_Type_create_resized(
-            matrix_block,
-            /* lower bound */ 0,
-            /* extent - size in bytes */ (int) (columns_per_process * sizeof(double)),
-            /* new type */ &matrix_block_resized
-    );
+    MPI_Datatype matrix_block_resized;
+    MPI_Type_create_resized(matrix_block, LOWER_BOUND,
+                            (int) (columns_per_process * sizeof(double)), &matrix_block_resized);
     MPI_Type_commit(&matrix_block_resized);
 
     int *sendcounts;
@@ -201,6 +160,11 @@ int run(void) {
     free(part_A);
     free(part_B);
     free(part_C);
+
+    const double end_time_s = MPI_Wtime();
+    if (RANK_ROOT == rank) {
+        printf("Time taken: %lf", end_time_s - start_time_s);
+    }
 
     return EXIT_SUCCESS;
 }
